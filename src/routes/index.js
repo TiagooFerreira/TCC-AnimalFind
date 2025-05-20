60e4562959221c8499aae3ce735381c6c9fdbe35
Tiago Ferreira <Tiagogoferreira14@gmail.com>
2025-05-20 10:16:17 -0300

    Atualizando Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Welcome from '../pages/welcome'
 import SignIn from '../pages/signIn'
-import mainPage from '../pages/mainPage'
 import Register from '../pages/register'
 import MainPage from '../pages/mainPage'
 
